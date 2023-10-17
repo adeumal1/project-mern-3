4b1b2fe1236cfbc786c9f14b2c8c5ecab420fb7b
adeumal1 <deumaltripianaaleix@gmail.com>
2023-10-17 11:24:43 +0200

    Adding game tic tac toe

--- src/App.jsx
+++ src/App.jsx
@@ -11,37 +11,46 @@ const SudokuPage = lazy(() => import("./pages/SudokuPage"));
 
 function App() {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/tictactoe">TicTacToe</Link>
-      <Link to="/hangman">Hangman</Link>
-      <Link to="/sudoku">Sudoku</Link>
-      <Routes>
-        <Route path="/" element={
-            <React.Suspense fallback={<OnLoad />}>
-              <HomePage />
-            </React.Suspense>
-          }
-        />
-        <Route path="/tictactoe" element={
-            <React.Suspense fallback={<OnLoad />}>
-              <TicTacToePage />
-            </React.Suspense>
-          }
-        />
-        <Route path="/hangman" element={
-            <React.Suspense fallback={<OnLoad />}>
-              <HangmanPage />
-            </React.Suspense>
-          }
-        />
-        <Route path="/sudoku" element={
-            <React.Suspense fallback={<OnLoad />}>
-              <SudokuPage />
-            </React.Suspense>
-          }
-        />
-      </Routes>
+    <div className="App">
+      <header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/tictactoe">TicTacToe</Link>
+          <Link to="/hangman">Hangman</Link>
+          <Link to="/sudoku">Sudoku</Link>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={
+              <React.Suspense fallback={<OnLoad />}>
+                <HomePage />
+              </React.Suspense>
+            }
+          />
+          <Route path="/tictactoe" element={
+              <React.Suspense fallback={<OnLoad />}>
+                <TicTacToePage />
+              </React.Suspense>
+            }
+          />
+          <Route path="/hangman" element={
+              <React.Suspense fallback={<OnLoad />}>
+                <HangmanPage />
+              </React.Suspense>
+            }
+          />
+          <Route path="/sudoku" element={
+              <React.Suspense fallback={<OnLoad />}>
+                <SudokuPage />
+              </React.Suspense>
+            }
+          />
+        </Routes>
+      </main>
+      <footer>
+        
+      </footer>
     </div>
     
   )
--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
-import './TicTacToe.css'
-const TicTacToe = () => {
-  return (
-    <div>TicTacToe</div>
-  )
-}
+import React, { useState, useEffect } from "react";
+import "./TicTacToe.css";
 
-export default TicTacToe
+const TicTacToe = () => {
+  const [isStarted, setIsStarted] = useState(false);
+  const [buttonText, setButtonText] = useState("✖️");
+  const [cells, setCells] = useState(Array(9).fill(null));
+  const [winner, setWinner] = useState(null);
+  const [classHiddenVisibleDiv, setClassHiddenVisibleDiv] = useState("hidden");
+  const [classHiddenVisibleP, setClassHiddenVisibleP] = useState("visible");
+
+  const winCombos = [
+    [0, 1, 2],[3, 4, 5],[6, 7, 8], // Rows
+    [0, 3, 6],[1, 4, 7],[2, 5, 8], // Columns
+    [0, 4, 8],[2, 4, 6], // Diagonals
+  ];
+
+  const handleStart = () => {
+    setIsStarted(!isStarted);
+    setCells(Array(9).fill(null));
+    setButtonText("✖️");
+    setWinner(null);
+    setClassHiddenVisibleDiv("hidden");
+    setClassHiddenVisibleP("visible");
+  };
+
+  const handlePlay = (index) => {
+    if (cells[index] === null) {
+      cells[index] = buttonText;
+      const newCells = [...cells];
+      setCells(newCells);
+      setButtonText(buttonText === "✖️" ? "⭕" : "✖️");
+    }
+  };
+
+  useEffect(() => {
+    // console.log('cells is changing');
+    // console.log(cells);
+    for (let i = 0; i < winCombos.length; i++) {
+      const [a, b, c] = winCombos[i];
+      console.log(cells[a], cells[b], cells[c]);
+      if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
+        setWinner(cells[a]);
+        setClassHiddenVisibleDiv("visible");
+        setClassHiddenVisibleP("hidden");
+      }
+    }
+  }, [cells]);
+
+  return (
+    <div className="container-tictactoe">
+      <h1>Tic Tac Toe</h1>
+      <button onClick={handleStart}>{isStarted ? "Restart" : "Start"}</button>
+      <p className={classHiddenVisibleP}>{isStarted ? "Turn for " + buttonText : ""}</p>
+      {isStarted ? (
+        <div>
+          <div className="table-tictactoe">
+            {cells.map((value, index) => (
+              <button
+                key={index}
+                className="cell"
+                data-cell={index + 1}
+                onClick={() => handlePlay(index)}
+              >
+                {value}
+              </button>
+            ))}
+          </div>
+          <div className={"winner-container " + classHiddenVisibleDiv}>
+            <h2>The winner is {winner}</h2>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h1>PRESS START</h1>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TicTacToe;
