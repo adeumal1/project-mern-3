fb8b54a397636b6698940fc5ab2d0b5c146d3731
adeumal1 <deumaltripianaaleix@gmail.com>
2023-11-27 18:57:15 +0100

    sudoku check solution new feature

--- src/components/Sudoku/Sudoku.jsx
+++ src/components/Sudoku/Sudoku.jsx
@@ -1,61 +1,90 @@
-import React, {useState, useEffect} from 'react'
-import { makepuzzle, solvepuzzle, ratepuzzle } from "sudoku";
-import play from "../../assets/img/play.svg";
-import pause from "../../assets/img/pause.svg";
-import './Sudoku.css'
+import React, { useState, useEffect } from 'react';
+import { makepuzzle, solvepuzzle } from 'sudoku';
+import play from '../../assets/img/play.svg';
+import pause from '../../assets/img/pause.svg';
+import './Sudoku.css';
 
 const Sudoku = () => {
-  
   const [isStarted, setIsStarted] = useState(false);
   const [sudokuBoard, setSudokuBoard] = useState(null);
+  const [solved, setSolved ]= useState(null);
+  const [selectedButton, setSelectedButton] = useState(null);
+  const numbersArray = Array.from(Array(9).keys()).map((x) => x + 1);
+  
+
+  const convertTo1To9 = (array) => array.map(cell => (cell !== null ? cell + 1 : null));
+
+  console.log(sudokuBoard);
+  console.log(solved);
+
+  useEffect(() => {
+    const newBoard = makepuzzle();
+    const newSolved = solvepuzzle(newBoard)
+    setSudokuBoard(convertTo1To9(newBoard));
+    setSolved(convertTo1To9(newSolved))
+  }, [isStarted]);
 
   const handleStart = () => {
     if (!isStarted) {
-      //rid of useStates
+      setSelectedButton(null);
     }
     setIsStarted(!isStarted);
   };
 
-  useEffect(() => {
-    const newBoard = makepuzzle();
-    setSudokuBoard(newBoard);
-  }, []);
   
-  console.log(sudokuBoard);
 
-  const renderBoard = () => {
-    if (!sudokuBoard) return null;
-    const board2D = [];
-    for (let i = 0; i < 9; i++) {
-      board2D.push(sudokuBoard.slice(i * 9, (i + 1) * 9));
+  const selectNumberToPrint = (e) => { 
+    if (selectedButton !== null) {
+      document.getElementById(`button${selectedButton}`).style.backgroundColor = '';
     }
-
-  const selectNumberToPrint = () => {
-
+    e.target.style.backgroundColor = 'lightblue';
+    setSelectedButton(e.target.value);
   };
 
-    return board2D.map((row, rowIndex) => (
-      <div key={rowIndex} className="row">
-        {row.map((cell, colIndex) => (
-          <div key={colIndex} className="cell">
-            {cell === 0 ? '' : cell}
-          </div>
-        ))}
-      </div>
-    ));
+  const updateBaord = (e) => {
+    if (e.target.innerHTML === '') { //bloquear solo los campos de la gneracion del board
+      const newBoard = [...sudokuBoard];
+      newBoard[e.target.value] = parseInt(selectedButton, 10);
+      setSudokuBoard(newBoard);
+    }
   };
 
-  return ( 
+  const checkWin = () => {
+  
+    if (JSON.stringify(sudokuBoard) === JSON.stringify(solved)) {
+      console.log("Son iguales!");
+    } else {
+      console.log('No son iguales.');
+    }
+  };
+
+  return (
     <div className="container-sudoku">
       <button className="play-button" onClick={handleStart}>
-        {isStarted ? "" : <img src={play} alt="" />}
+        {isStarted ? '' : <img src={play} alt="" />}
       </button>
       {isStarted ? (
         <div className="content-container-sudoku">
-          <div className="content-button-selector"></div>
-          <div className="board">{renderBoard()}</div>
+          <div className="content-button-selector">
+            {numbersArray.map((number, index) => (
+              <button
+                key={index}
+                id={`button${number}`}
+                onClick={selectNumberToPrint}
+                value={number}
+              >
+                {number}
+              </button>
+            ))}
+          </div>
+          <div className="board">
+            {sudokuBoard.map((number, index) => (
+              <button key={index} className={number === null ? "cell2" : "cell2 defaultnumber"} value={index} onClick={updateBaord}>{number}</button>
+             ))}
+          </div>
+          <button className="win-button" onClick={checkWin}>check solution</button>
           <button className="pause-button" onClick={handleStart}>
-              {isStarted ? <img src={pause} alt="" /> : ""}
+            {isStarted ? <img src={pause} alt="" /> : ''}
           </button>
         </div>
       ) : (
@@ -67,4 +96,4 @@ const Sudoku = () => {
   );
 };
 
-export default Sudoku
+export default Sudoku;
